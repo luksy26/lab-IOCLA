ee534053c3f18b6552d879555e941d40cb0c7872
elenaioana <73949100+elenaioana@users.noreply.github.com>
2021-10-22 20:19:36 +0300

    Add files via upload

---
+++ ex3.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include "pixel.h"
+#define GET_PIXEL(a, i ,j)  (*(*(a + i) + j))#define GET_PIXEL(a, i ,j)  (*(*(a + i) + j))
+
+
+/*
+	TODO a
+	Functia primeste ca parametru o imagine si intoarce imaginea rasturnata.
+	Prin imagine rasturnata se intelege inversarea liniilor matricei pix_array
+	din structura lui Picture, astfel: Linia 1 devine linia n, linia 2 devine
+	linia n - 1, etc.
+*/
+void reversePic(Picture *pic);
+
+void reversePic(Picture *pic) {
+	
+	Picture *reversed = generatePicture(pic->height,pic-> width,pic-> pix_array);
+	for(int i = 0; i < pic->height; i++){
+		for(int j = 0; j < pic->width; j++){
+			reversed->pix_array[i][j] = pic->pix_array[pic->height - 1 - i][j];
+		}
+	}
+	
+	
+	printPicture(reversed);
+	freePixelArray(&reversed->pix_array, reversed->height, reversed->width);
+	freePicture(&reversed);
+	
+}
+
+/*
+	TODO b
+	Functia primeste ca parametru o imagine si intoarce noua imagine obtinuta
+	prin convertirea fiecarui pixel la valoarea sa grayscale.
+	Valoarea grayscale a unui pixel sa calculeaza dupa urmatoarea formula:
+	p.r = 0.3 * p.r;
+	p.g = 0.59 * p.g;
+	p.b = 0.11 * p.b;
+
+	
+*/
+
+void colorToGray(Picture *pic);
+
+/*
+	Structura unui pixel, cea a unei imagini, precum si generarea acestora
+	sunt definite in pixel.h. Programul primeste de la tastatura inaltimea
+	si latimea imaginii. De preferat, introduceti valori mici pentru
+	a fi usor de verificat ulterior.
+	Folositi functia printPicture pentru a printa componentele imaginii.
+	Dupa ce ati realizat un TODO, apelati functia corespunzatoare in main
+	urmata de printPicture pentru a vedea daca se obtine rezultatul dorit.
+*/
+
+void colorToGray(Picture *pic){
+		
+		for(int i = 0; i < pic->height; i++){
+			for(int j = 0; j < pic->width; j++){
+				GET_PIXEL(pic,i, j).R*=0.3;
+				GET_PIXEL(pic,i, j).B*=0.59;
+				GET_PIXEL(pic,i, j).G*=0.11;
+			}
+		}
+		printPicture(pic);
+}
+
+int main() {
+	int height, width;
+	scanf("%d%d", &height, &width);
+	Pixel **pix_array = generatePixelArray(height, width);
+	Picture *pic = generatePicture(height, width, pix_array);
+
+	printPicture(pic);
+
+	reversePic(pic);
+
+	colorToGray(pic);
+	
+	freePicture(&pic);
+	freePixelArray(&pix_array, height, width);
+
+	return 0;
+}
