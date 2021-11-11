b241a3ead4592a30b7e9e306b689c0e85ab6bfb8
elenaioana <73949100+elenaioana@users.noreply.github.com>
2021-11-11 18:12:36 +0200

    Add files via upload

---
+++ min.asm
@@ -0,0 +1,19 @@
+%include "../io.mac"
+
+section .text
+    global main
+    extern printf
+
+main:
+    ;cele doua numere se gasesc in eax si ebx
+    mov eax, 4
+    mov ebx, 1
+    ; TODO: aflati minimul
+    cmp eax, ebx
+    jl print
+    xchg eax, ebx
+
+print :
+    PRINTF32 `%d\n\x0`, eax ; afiseaza minimul
+
+    ret
