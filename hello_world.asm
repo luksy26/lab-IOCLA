b3d3431f10c56443ed94d983f4ec86ad978add44
luksy26 <73948966+luksy26@users.noreply.github.com>
2021-11-11 17:31:36 +0200

    ex2
    more hellos and goodbye

---
+++ hello_world.asm
@@ -0,0 +1,31 @@
+%include "../io.mac"
+
+section .data
+    myString: db "Hello, World!", 0
+    myString2: db "Goodbye, World!", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    mov ecx, 6                      ; N = valoarea registrului ecx
+    mov eax, 2
+    mov ebx, 1
+    cmp eax, ebx
+    jg print                        ; TODO1: eax > ebx?
+    ret
+
+print:
+    PRINTF32 `%s\n\x0`, myString
+    sub ecx, 1
+    cmp ecx, 0
+    jg print
+    
+print2:
+    PRINTF32 `%s\n\x0`, myString2
+
+                                    ; TODO2.2: afisati "Hello, World!" de N ori
+                                    ; TODO2.1: afisati "Goodbye, World!"
+
+    ret
